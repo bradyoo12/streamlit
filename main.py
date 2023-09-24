06dd702d134de24fbd328c8bec4448b3801c1c8e
Brad Yoo <byoo@janison.com>
2023-09-24 23:32:52 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=kHz6sz0jVSU
+import streamlit as st
+
+headerSection = st.container()
+mainSection = st.container()
+leftNav = st.sidebar
+
+with headerSection:
+    st.title("Streamlit")
+    st.markdown("This is a demo of Streamlit")
+    
